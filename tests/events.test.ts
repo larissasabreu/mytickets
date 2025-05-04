03bb55078de928a1d1f73a83bcfea8e850564444
Larissa Abreu <larissasabreux@gmail.com>
2025-05-04 20:35:01 -0300

    C:/Program Files/Git/events

--- tests/events.test.ts
+++ tests/events.test.ts
@@ -3,6 +3,8 @@ import supertest from "supertest"
 import { createNewEventData } from "./factories/events-factory";
 import prisma from "database";
 import httpStatus from "http-status";
+import { faker } from '@faker-js/faker';
+import { findEventById, findEventByName } from "repositories/events-repository";
 
 const api = supertest(app);
 
@@ -34,7 +36,7 @@ describe("GET /events", () => {
 
 // eventsRouter.get("/events/:id", getEvent);
 
-describe("GET /events:id", () => {
+describe("GET /events/:id", () => {
   it("should return status 200 and an array", async () => {
       const event = await createNewEventData();
 
@@ -57,5 +59,73 @@ describe("GET /events:id", () => {
     });
 
 // eventsRouter.post("/events", validateSchema(eventSchema), postEvent);
+describe("POST /events", () => {
+  it("should return status 200 and an array", async () => {
+      const newEvent = {
+        name: faker.word.noun(),
+        date: faker.date.future()
+      };
+
+      const response = await api.post("/events").send(newEvent);
+      const result = await findEventByName(newEvent.name)
+  
+      expect(response.status).toBe(201);
+
+      expect(result).toEqual(
+        expect.objectContaining({
+          id: result.id,
+          name: newEvent.name,
+          date: newEvent.date
+        })
+      );
+    });
+    });
+
 // eventsRouter.put("/events/:id", validateSchema(eventSchema), putEvent);
+describe("PUT /events/:id", () => {
+  it("should return status 200 and an array", async () => {
+      const event = await createNewEventData();
+
+      const editEvent = {
+        name: faker.word.noun(),
+        date: faker.date.future()
+      };
+
+      const response = await api.put(`/events/${event.id}`).send(editEvent);
+      const result = await findEventById(event.id)
+  
+      expect(response.status).toBe(200);
+
+      expect(result).toEqual(
+        expect.objectContaining({
+          id: event.id,
+          name: editEvent.name,
+          date: editEvent.date
+        })
+      );
+    });
+      it("should return 422", async () => {
+        const response = await api.put("/events/-1");
+    
+        expect(response.status).toBe(422);
+      });
+    });
+
 // eventsRouter.delete("/events/:id", deleteEvent);
+describe("DELETE /events/:id", () => {
+  it("should return status 200 and an array", async () => {
+      const event = await createNewEventData();
+
+      const response = await api.delete(`/events/${event.id}`);
+      const result = await findEventById(event.id)
+  
+      expect(response.status).toBe(204);
+
+      expect(result).toBeNull();
+    });
+      it("should return 400", async () => {
+        const response = await api.delete("/events/-1");
+    
+        expect(response.status).toBe(400);
+      });
+    });
