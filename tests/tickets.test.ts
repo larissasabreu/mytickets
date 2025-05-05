89091bd39388058abd2511acb64b0a1c07a8abfa
Larissa Abreu <larissasabreux@gmail.com>
2025-05-04 21:09:39 -0300

    C:/Program Files/Git/tickets test

--- tests/tickets.test.ts
+++ tests/tickets.test.ts
@@ -3,7 +3,9 @@ import supertest from "supertest"
 import { createNewTicketData } from "./factories/tickets-factory";
 import { createNewEventData } from "./factories/events-factory";
 import prisma from "database";
+import { faker } from '@faker-js/faker';
 import httpStatus from "http-status";
+import { findTicketByCodeForEvent } from "repositories/tickets-repository";
 
 
 const api = supertest(app);
@@ -32,7 +34,7 @@ describe("GET /tickets/:eventId", () => {
         );
       });
         it("should return 400", async () => {
-            const response = await api.get("/events/-1");
+            const response = await api.get("/tickets/-1");
         
             expect(response.status).toBe(400);
         });
@@ -41,4 +43,42 @@ describe("GET /tickets/:eventId", () => {
         
 
 //ticketsRouter.post("/tickets", validateSchema(ticketSchema), postTicket);
+
+describe("POST /tickets", () => {
+    it("should return status 200 and an array", async () => {
+        const event = await createNewEventData();
+
+        const newTicket = {
+           owner: faker.person.fullName(),
+           code: faker.string.alpha(),
+           eventId: event.id
+        };
+  
+        const response = await api.post("/tickets").send(newTicket);
+        const result = await findTicketByCodeForEvent(event.id, newTicket.code)
+    
+        expect(response.status).toBe(201);
+  
+        expect(result).toEqual(
+          expect.objectContaining({
+            owner: newTicket.owner,
+            code: newTicket.code,
+            eventId: event.id
+          })
+        );
+      });
+      });
+
 //ticketsRouter.put("/tickets/use/:id", putTicket);
+
+describe("PUT /tickets/use/:id", () => {
+    it("should return status 200 and an array", async () => {
+        const event = await createNewEventData();
+        const ticket = await createNewTicketData();
+
+        const response = await api.put(`/tickets/use/${ticket.id}`);
+    
+        expect(response.status).toBe(204);
+
+        });
+      });
